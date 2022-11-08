434257078dbf79c9abb970fe9095cc57f545cb6e
Clarissa Santos crisostomo <csantos-@c1r10p1.42sp.org.br>
2022-11-08 20:42:30 -0300

    fixed segfault

--- CPP01/ex01/main.cpp
+++ CPP01/ex01/main.cpp
@@ -1,9 +1,12 @@
 #include "./Zombie.hpp"
 
 int main(void){
-    Zombie *horde = zombieHorde(5,"clarivader");
-    for (int i = 0; i < 5; i++)
+    Zombie *horde = zombieHorde(40,"clarivader");
+    for (int i = 0; i < 40; i++)
+    {
+        std::cout << "[Zombie " << i + 1 << "] ";
         horde->announce();
+    }
     delete[] horde;
     return (0);
 }
--- CPP01/ex01/zombieHorde.cpp
+++ CPP01/ex01/zombieHorde.cpp
@@ -3,7 +3,7 @@
 Zombie* zombieHorde(int n, std::string name){
     Zombie *horde = new Zombie[n];
     
-    for (int i = 0; i <= n; i++){
+    for (int i = 0; i < n; i++){
         horde[i].setName(name);
     }
     return horde;
