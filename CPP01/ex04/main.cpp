0aab9c5815da94bc24362c07da80ed446a095542
clarissa <clarissa@42sp.org.br>
2023-11-25 16:19:53 -0300

    mindblown by ex04

--- CPP01/ex04/main.cpp
+++ CPP01/ex04/main.cpp
@@ -1,12 +1,33 @@
 #include <iostream>
 #include <fstream>
 #include <sstream>
+#include <cstdlib>
 
 int main ( int argc, char **argv ){
 
-    std::ifstream   ifs("arquivo");
-    std::string     s1;
-    std::string     s2;
+    std::ifstream   from(argv[1]);
+    std::string filename = argv[1];
+    std::string  s1 = argv[2];
+    std::string s2 = argv[3];
+    std::ofstream to(filename.append(".replace"));
+    std::ostringstream tmp;
+    std::string tmp2;
+    std::size_t found = 0;
 
-    ifs >> s1 >> s2;
+    if (argc !=4)
+        return (1);
+
+    tmp << from.rdbuf();
+    tmp2 = tmp.str();
+
+    from.close();
+    while ((found = tmp2.find(s1, found)) != std::string::npos)
+    {
+        tmp2.erase(found, s1.length());
+        tmp2.insert(found, s2);
+        found += s2.length();
+    }
+    
+    to << tmp2;
+    to.close();
 }
